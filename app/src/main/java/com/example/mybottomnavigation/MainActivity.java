4bba8608e47b6e99e5a607e4ada09ce3358e5654
ALFIAN SATRIA V.P <masfian77@gmail.com>
2019-09-16 17:17:55 +0700

    committed

---
+++ app/src/main/java/com/example/mybottomnavigation/MainActivity.java
@@ -0,0 +1,63 @@
+package com.example.mybottomnavigation;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+import com.example.mybottomnavigation.ui.dashboard.DashboardFragment;
+import com.example.mybottomnavigation.ui.home.HomeFragment;
+import com.example.mybottomnavigation.ui.notifications.NotificationsFragment;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
+public class MainActivity extends AppCompatActivity {
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            Fragment fragment;
+
+            switch (item.getItemId()) {
+                case R.id.navigation_home:
+
+                    fragment = new HomeFragment();
+                    getSupportFragmentManager().beginTransaction()
+                            .replace(R.id.container_layout, fragment, fragment.getClass().getSimpleName())
+                            .commit();
+                    return true;
+                case R.id.navigation_dashboard:
+
+                    fragment = new DashboardFragment();
+                    getSupportFragmentManager().beginTransaction()
+                            .replace(R.id.container_layout, fragment, fragment.getClass().getSimpleName())
+                            .commit();
+                    return true;
+                case R.id.navigation_notifications:
+
+                    fragment = new NotificationsFragment();
+                    getSupportFragmentManager().beginTransaction()
+                            .replace(R.id.container_layout, fragment, fragment.getClass().getSimpleName())
+                            .commit();
+                    return true;
+            }
+            return false;
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        BottomNavigationView navigation = findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+
+        if (savedInstanceState == null){
+            navigation.setSelectedItemId(R.id.navigation_home);
+        }
+
+    }
+
+}
---
+++ app/src/main/java/com/example/mybottomnavigation/ui/notifications/NotificationsViewModel.java
@@ -0,0 +1,19 @@
+package com.example.mybottomnavigation.ui.notifications;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class NotificationsViewModel extends ViewModel {
+
+    private MutableLiveData<String> mText;
+
+    public NotificationsViewModel() {
+        mText = new MutableLiveData<>();
+        mText.setValue("Selamat datang di halaman Notifications");
+    }
+
+    public LiveData<String> getText() {
+        return mText;
+    }
+}
